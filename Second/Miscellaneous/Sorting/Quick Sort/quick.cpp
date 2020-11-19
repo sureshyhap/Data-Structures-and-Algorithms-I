1c814e334dfd045dc5ff1977221ece9434fee0f7
Suresh Yhap <sureshyhap@gmail.com>
2020-11-19 11:55:04 -0500

    Optimized quick sort except for median of three

--- Second/Miscellaneous/Sorting/Quick Sort/quick.cpp
+++ Second/Miscellaneous/Sorting/Quick Sort/quick.cpp
@@ -7,6 +7,9 @@ void quick_sort(typename std::vector<T>& lyst, int start, int end);
 template <typename T>
 inline void swap(T& first, T& second);
 
+template <typename T>
+void insertion_sort(std::vector<T>& lyst, int start, int end);
+
 int main(int argc, char* argv[]) {
   std::vector<int> v = {4, 1, 3, 6, 78, 3, 5, 2, 6, 3, 23, 1};
   quick_sort(v, 0, v.size() - 1);
@@ -19,8 +22,10 @@ int main(int argc, char* argv[]) {
 
 template <typename T>
 void quick_sort(typename std::vector<T>& lyst, int start, int end) {
-  if (start >= end) {
-    return;
+  static int insertion_cutoff = 5;
+ beginning:
+  if (end - start < insertion_cutoff) {
+    return insertion_sort(lyst, start, end);
   }
   int mid_index = {(start + end) / 2};
   T pivot = {lyst.at(mid_index)};
@@ -30,7 +35,7 @@ void quick_sort(typename std::vector<T>& lyst, int start, int end) {
     while (lyst.at(++i) < pivot) {
       ;
     }
-    while (j >= 0 and lyst.at(--j) > pivot) {
+    while (j >= i and lyst.at(--j) > pivot) {
       ;
     }
     if (i < j) {
@@ -39,7 +44,8 @@ void quick_sort(typename std::vector<T>& lyst, int start, int end) {
   }
   swap(lyst.at(i), lyst.at(end));
   quick_sort(lyst, start, i - 1);
-  quick_sort(lyst, i + 1, end);
+  start = i + 1;
+  goto beginning;
 }
 
 template <typename T>
@@ -47,4 +53,22 @@ inline void swap(T& first, T& second) {
   T temp = first;
   first = second;
   second = temp;
+}
+
+template <typename T>
+void insertion_sort(std::vector<T>& lyst, int start, int end) {
+  int length = {end - start + 1};
+  for (int i = {start + 1}; i < start + length; ++i) {
+    T data = {lyst.at(i)};
+    int j = {i - 1};
+    for (; j >= start; --j) {
+      if (lyst.at(j) > data) {
+	lyst.at(j + 1) = {lyst.at(j)};
+      }
+      else {
+	break;
+      }
+    }
+    lyst.at(j + 1) = data;
+  }
 }
